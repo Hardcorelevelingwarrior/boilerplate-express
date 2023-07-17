e6d6ba409205f420c21d0cd81c44d071fadddb92
Hardcorelevelingwarrior <nhuducminh123@gmail.com>
2023-07-17 14:43:55 +0700

    Serving json data

--- myApp.js
+++ myApp.js
@@ -2,12 +2,14 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 //app.get("/",function(req,res){res.send("Hello Express")});
+//Request for HTML
 absoulutePathHTML=__dirname + "/views/index.html";
 app.get("/",function(req,res){res.sendFile(absoulutePathHTML)});
-
+//Request for assests like css
 absoulutePathAssets=__dirname + "/public";
 app.use("/public",express.static(absoulutePathAssets));
-
+//Request for json data
+app.get("/json",function(req,res){res.json({"message": "Hello json"})})
 
 
 
