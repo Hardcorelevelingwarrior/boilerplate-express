63c8c4036432222ee36b5319e7ac9cb492649576
Hardcorelevelingwarrior <nhuducminh123@gmail.com>
2023-07-17 15:33:41 +0700

    Take parameters as input

--- myApp.js
+++ myApp.js
@@ -15,10 +15,19 @@ app.use(function middleware(req,res,next){
     res.send(req.method+" "+req.path+" - "+req.ip);
     next();
 })
+//Using chain to return current time
+app.get("/now",function(req,res,next){
+    req.time= new Date().toString();
+    next();
+  },function middleware(req,res){
+    app.send(res.json({"time":req.time}))
+  })
 
-
-
-
+//Using : to take route parameter input
+  app.get("/:word/echo",function(req,res){
+    word=req.params.word
+    app.send(res.json({"echo":word}))
+  })
 
 
 
